776238642585209e4f77178047dac72d848e8cb3
matheuskkg <matheus.kkg21@gmail.com>
2024-08-21 18:02:47 -0300

    .

--- src/main/java/com/brasa/user/UserValidator.java
+++ src/main/java/com/brasa/user/UserValidator.java
@@ -12,7 +12,7 @@ public class UserValidator {
     private ResponseEntity<String> response;
 
     public boolean isUsernameValid(String username) {
-        if (!Pattern.matches("^[a-zA-Z0-9]{3,16}$", username)) {
+        if (!Pattern.matches("^[a-zA-Z0-9]{1,16}$", username)) {
             this.response = ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
                     .body("The username must be alphanumeric, cannot contain spaces, and must be between 1 and 16 characters in length.");
