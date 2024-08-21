da3e30c126f204d1a016ccc8fb639065ce98d59f
matheuskkg <matheus.kkg21@gmail.com>
2024-08-21 18:01:15 -0300

    use regex only and return generic message if bad request

--- src/main/java/com/brasa/user/UserValidator.java
+++ src/main/java/com/brasa/user/UserValidator.java
@@ -12,46 +12,10 @@ public class UserValidator {
     private ResponseEntity<String> response;
 
     public boolean isUsernameValid(String username) {
-        //I find it easier to read it like this than to simply return the conditions
-        if (isUsernameEmpty(username)
-                || isUsernameOutOfBounds(username)
-                || !usernameMatchesRegex(username)
-        ) {
-            return false;
-        }
-
-        return true;
-    }
-
-    private boolean isUsernameEmpty(String username) {
-        if (username == null || username.isBlank()) {
+        if (!Pattern.matches("^[a-zA-Z0-9]{3,16}$", username)) {
             this.response = ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
-                    .body("Username cannot be empty.");
-
-            return true;
-        }
-
-        return false;
-    }
-
-    private boolean isUsernameOutOfBounds(String username) {
-        if (username.length() > 16) {
-            this.response = ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body("Username maximum length is 16.");
-
-            return true;
-        }
-
-        return false;
-    }
-
-    private boolean usernameMatchesRegex(String username) {
-        if (!Pattern.matches("^[a-zA-Z0-9]{1,16}$", username)) {
-            this.response = ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body("Username contains invalid character.");
+                    .body("The username must be alphanumeric, cannot contain spaces, and must be between 1 and 16 characters in length.");
 
             return false;
         }
